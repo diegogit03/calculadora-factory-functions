38a95162af4e614138df64140d6025dd356c0bc4
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-08-21 11:06:42 -0300

    feature delete one

--- main.js
+++ main.js
@@ -2,19 +2,37 @@ function criaCalculadora(){
     return {
 
         display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
 
+        
         start(){
             this.clickButtons();
         },
-
+        
         clickButtons(){
             document.addEventListener('click', (event)=>{
                 const element = event.target;
-
+                
                 if(element.classList.contains('btn-num')){
                     this.btnToDisplay(element.innerHTML);
                 };
+                
+                if(element.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                };
+                
+                if(element.classList.contains('btn-del')){
+                    this.deleteOne();
+                };
             });
+        },
+        
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        deleteOne(){
+            this.display.value = this.display.value.slice(0, -1)
         },
 
         btnToDisplay(value){
